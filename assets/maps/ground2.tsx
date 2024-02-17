896d896e889ab8c0dc1fc9b808e72004a34002fd
Filip Pivarci <filip.pivarci@gmail.com>
2024-02-18 00:58:00 +0900

    build

---
+++ assets/maps/ground2.tsx
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="ground" tilewidth="64" tileheight="64" tilecount="11" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../ground/grass3.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../ground/grass4.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../ground/grass5.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../ground/sand1.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="../ground/sand2.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="../ground/sand3.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="../ground/wood1.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="../ground/wood2.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="../ground/wood3.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="../ground/grass1.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="../ground/grass2.png"/>
+ </tile>
+</tileset>
---
+++ assets/maps/objects2.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="objects" tilewidth="64" tileheight="64" tilecount="15" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../objects/0.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../objects/1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../objects/2.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../objects/3.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="../objects/4.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="../objects/5.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="../objects/6.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="../objects/7.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="../objects/8.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="../objects/9.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="../objects/10.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="../objects/11.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="../objects/12.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="../objects/13.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="../objects/14.png"/>
+ </tile>
+</tileset>
